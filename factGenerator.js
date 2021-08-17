74571cbe3d6e758b120cddeab47452f8c94d3537
Eric Duwe <ericduwe@gmail.com>
2021-08-16 23:05:41 -0500

    added category and subcategory context for facts, HTML header for container

--- factGenerator.js
+++ factGenerator.js
@@ -33,6 +33,10 @@ fetch(factUrl)
         .then(function (data) {
             console.log(data)
 
+			var factCategory = document.createElement("h3");
+            factCategory.textContent = "Category: " + data.contents.category + ", " + data.contents.subcategory
+            factContainer.appendChild(factCategory);
+
 			var fact = document.createElement("h3");
             fact.textContent = data.contents.fact
             factContainer.appendChild(fact);

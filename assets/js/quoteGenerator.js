b83e37b22d23d437aeca37793122ca2849c3e6bc
Eric Duwe <ericduwe@gmail.com>
2021-11-01 14:28:55 -0500

    fixed API endpoints

--- assets/js/quoteGenerator.js
+++ assets/js/quoteGenerator.js
@@ -3,8 +3,14 @@ let button=document.querySelector("#jokeButton")
 button.addEventListener("click", getJoke)
 
 function getJoke(){
-    fetch("https://official-joke-api.appspot.com/random_joke")
-      .then((response) => {return response.json()})
+
+  fetch("https://jokeapi-v2.p.rapidapi.com/joke/Any?safe-mode&format=json&contains=C%2523&idRange=0-150&blacklistFlags=nsfw%2Cracist", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "jokeapi-v2.p.rapidapi.com",
+      "x-rapidapi-key": "3c92b776ecmsh9653242e81dc867p11b153jsn8407c6570d0e"
+    }
+  }) .then((response) => {return response.json()})
       .then((data) => {renderJoke(data)
       console.log(data)
   });
@@ -15,5 +21,5 @@ function getJoke(){
     const setup = document.getElementById("setup");
     const punchline = document.getElementById("punchline");
     setup.textContent = data.setup
-    punchline.textContent = data.punchline
+    punchline.textContent = data.delivery
   }
